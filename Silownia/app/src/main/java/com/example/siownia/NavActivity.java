b7a1e53055caefa24f19689a4b1f798b7ac5b23b
User52525252 <90308361+User52525252@users.noreply.github.com>
2022-02-02 13:08:02 +0100

    Praca grupowa
    A

--- Silownia/app/src/main/java/com/example/siownia/NavActivity.java
+++ Silownia/app/src/main/java/com/example/siownia/NavActivity.java
@@ -2,7 +2,9 @@ package com.example.siownia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class NavActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class NavActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nav);
+    }
+
+    public void wyloguj(View view) {
+        {
+            Intent switchActivityIntent = new Intent(this, MainActivity.class);
+            startActivity(switchActivityIntent);
+        }
     }
 }
